98cef26860a764776c5206b32dd1220dd52a2fbe
Bamieh <ahmadbamieh@gmail.com>
2017-11-09 18:16:09 +0200

    example

---
+++ serverless-example.js
@@ -0,0 +1,21 @@
+const serverlessTransmission = require('winston-transmission/serverless');
+
+module.exports.someEvent = function(event, context, callback) {
+  context.log("some information");
+  callback(null, "Some event");
+}
+
+module.exports.anoherEvent = function(event, context, callback) {
+  callback(null, "Anoher event");
+}
+
+module.exports = serverlessTransmission({
+  exports: module.exports,
+  extraLoggingContext: (event, context) => {
+    return {
+      user: {
+        customer: JSON.parse(event.body).customer;
+      },
+    }
+  }
+})
